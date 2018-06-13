b4ed0272577c640c424f4fa0de2e53e78413f191
Albert Villaruz <Albert.Villaruz@GeorgianCollege.ca>
2018-06-13 11:44:37 -0400

    added table and add form

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,5 @@ router.get('/admin', gamesController.admin);
 router.get('/playgame', gamesController.playGame);
 
 router.get('/add', gamesController.addGame);
-// router.post('/add', gamesController.createGame);
 
 module.exports = router;
